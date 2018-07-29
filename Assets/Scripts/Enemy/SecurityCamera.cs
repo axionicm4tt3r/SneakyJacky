0603b790bf617647d621022d3e0f86875313a017
Marcel Roodt <marcelroodt@gmail.com>
2018-07-29 22:38:33 +0200

    Camera Improvements, Basic Alarm Manager

---
+++ Assets/Scripts/Enemy/SecurityCamera.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using SensorToolkit;
+using System.Linq;
+
+public class SecurityCamera : MonoBehaviour
+{
+	public float RotationSpeed;
+	public float ScanTime;
+	public float TrackTime;
+	public float ScanArcAngle;
+	public Light SpotLight;
+	public Sensor Sensor;
+	public Color ScanColour;
+	public Color TrackColour;
+	public Color AlarmColour;
+
+	Quaternion leftExtreme;
+	Quaternion rightExtreme;
+	Quaternion targetRotation;
+
+	void Awake()
+	{
+		leftExtreme = Quaternion.AngleAxis(ScanArcAngle / 2f, Vector3.up) * transform.rotation;
+		rightExtreme = Quaternion.AngleAxis(-ScanArcAngle / 2f, Vector3.up) * transform.rotation;
+	}
+
+	void OnEnable()
+	{
+		targetRotation = transform.rotation;
+		transform.rotation = rightExtreme;
+		StartCoroutine(ScanState());
+	}
+
+	void Update()
+	{
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+	}
+
+	IEnumerator ScanState()
+	{
+		StartCoroutine(ScanMovement());
+		while (true)
+		{
+			if (AlarmManager.Instance.IsAlarmState)
+			{
+				StopAllCoroutines();
+				StartCoroutine(AlarmState());
+				break;
+			}
+			else if (GetSpottedEnemy() != null)
+			{
+				StopAllCoroutines();
+				StartCoroutine(TrackState());
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	IEnumerator ScanMovement()
+	{
+		SpotLight.color = ScanColour;
+		while (true)
+		{
+			targetRotation = leftExtreme;
+			yield return new WaitForSeconds(ScanTime);
+			targetRotation = rightExtreme;
+			yield return new WaitForSeconds(ScanTime);
+		}
+	}
+
+	IEnumerator TrackState()
+	{
+		SpotLight.color = TrackColour;
+		var enemy = GetSpottedEnemy();
+		var timer = 0f;
+		while (Sensor.IsDetected(enemy))
+		{
+			targetRotation = Quaternion.LookRotation(transform.position - enemy.transform.position, Vector3.up);
+			timer += Time.deltaTime;
+			if (timer >= TrackTime)
+			{
+				AlarmManager.Instance.StartAlarm(enemy);
+				StopAllCoroutines();
+				StartCoroutine(AlarmState());
+				break;
+			}
+			yield return null;
+		}
+		StopAllCoroutines();
+		StartCoroutine(ScanState());
+	}
+
+	IEnumerator AlarmState()
+	{
+		targetRotation = transform.rotation;
+		SpotLight.color = AlarmColour;
+		yield return null;
+	}
+
+	GameObject GetSpottedEnemy()
+	{
+		var entities = Sensor.GetDetectedByComponent<Player>();
+		return entities.FirstOrDefault()?.gameObject;
+	}
+}
---
+++ Assets/Scripts/GameManagers/AlarmManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmManager : MonoBehaviour
+{
+	//public Light PointLight;
+	public Color AlarmColour;
+	public float FlashFrequency;
+	public Transform[] LinearLevelWaypoints;
+
+	bool alarmStarted = false;
+	GameObject whoTrippedAlarm;
+
+	public static AlarmManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<AlarmManager>();
+			}
+			return instance;
+		}
+	}
+	private static AlarmManager instance;
+
+	public bool IsAlarmState
+	{
+		get { return alarmStarted && WhoTrippedAlarm != null; }
+	}
+
+	public GameObject WhoTrippedAlarm
+	{
+		get { return whoTrippedAlarm; }
+	}
+
+	public void StartAlarm(GameObject whoTrippedAlarm)
+	{
+		if (!alarmStarted)
+		{
+			this.whoTrippedAlarm = whoTrippedAlarm;
+			StartCoroutine(AlarmRoutine());
+		}
+	}
+
+	/*
+	 * Extremely basic pathfinding which assumes a linear level layout. Finds the closest waypoint index to
+	 * 'from' and to the 'whoTrippedAlarm' gameobjects. The path is then the sequence of waypoint indices between
+	 * these two in LinearLevelWaypoints. 
+	 */
+	public Transform[] PathToWhoTrippedAlarm(GameObject from)
+	{
+		var nearestToFrom = GetNearestWaypointIndex(from.transform.position);
+		var nearestToTripper = GetNearestWaypointIndex(whoTrippedAlarm.transform.position);
+		var nWaypoints = Mathf.Abs(nearestToFrom - nearestToTripper);
+		bool ascending = nearestToTripper > nearestToFrom;
+		var path = new Transform[nWaypoints];
+		for (int i = 0; i < nWaypoints; i++)
+		{
+			var offset = ascending ? i : -i;
+			path[i] = LinearLevelWaypoints[nearestToFrom + offset];
+		}
+
+		return path;
+	}
+
+	IEnumerator AlarmRoutine()
+	{
+		alarmStarted = true;
+		//PointLight.color = AlarmColour;
+		//var startIntensity = PointLight.intensity;
+
+		while (true)
+		{
+			//var intensity = (Mathf.Sin(FlashFrequency * Time.time * Mathf.PI * 2f) + 1f) / 2f * startIntensity;
+			//PointLight.intensity = intensity;
+			yield return null;
+		}
+	}
+
+	int GetNearestWaypointIndex(Vector3 toPos)
+	{
+		float nearestDist = 0f;
+		int nearest = -1;
+		for (int i = 0; i < LinearLevelWaypoints.Length; i++)
+		{
+			var dist = (toPos - LinearLevelWaypoints[i].position).sqrMagnitude;
+			if (dist < nearestDist || nearest == -1)
+			{
+				nearest = i;
+				nearestDist = dist;
+			}
+		}
+		return nearest;
+	}
+}
